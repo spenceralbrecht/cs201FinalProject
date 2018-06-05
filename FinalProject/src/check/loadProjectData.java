63601c6087e5ee08ae6cdf7ca75d5c8a99803eb8
Spencer Albrecht <spencera@usc.edu>
2018-06-04 19:44:49 -0700

    Remove JSON interfacing code from project servlet

--- FinalProject/src/check/loadProjectData.java
+++ FinalProject/src/check/loadProjectData.java
@@ -10,10 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import backend.Database;
 
 /**
  * Servlet implementation class loadProjectData
@@ -35,38 +31,6 @@ public class loadProjectData extends HttpServlet {
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		protected Database getDatabase() {
-			String file = null;
-			BufferedReader reader = null;
-			Gson gson = null;
-			Database database = null;
-		
-			try {
-				// Try to read in the JSON file
-				file = this.getServletContext().getRealPath("Sample.json");
-				reader = new BufferedReader(new FileReader(file));
-				gson = new GsonBuilder().setPrettyPrinting().create();
-				database = gson.fromJson(reader, Database.class);
-				
-				return database;
-			} 
-			catch (com.google.gson.JsonSyntaxException e) {
-				System.out.println("File is not well-formed JSON.");
-				System.out.println();
-				return database;	
-			}
-			catch (com.google.gson.JsonIOException e) {
-				System.out.println("JSON file could not be read.");
-				System.out.println();
-				return database;	
-			}
-			catch (FileNotFoundException e) {
-				System.out.println("File not found.");
-				System.out.println();
-				return database;	
-			}
-			
-		}
 	}
 
 }
