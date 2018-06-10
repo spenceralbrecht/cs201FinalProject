b2da63b5e2ef737d1c9b33e2504883e83bf60ea9
Spencer Albrecht <spencera@usc.edu>
2018-06-10 13:43:47 -0700

    Fixing JDBC driver null pointer

--- FinalProject/src/database/LoadProjectData.java
+++ FinalProject/src/database/LoadProjectData.java
@@ -1,6 +1,21 @@
 package database;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class LoadProjectData
