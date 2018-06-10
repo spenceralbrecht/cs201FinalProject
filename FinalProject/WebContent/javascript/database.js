124f86dd80e3c3bdc28518b5a30e92ed5d3ac726
Spencer Albrecht <spencera@usc.edu>
2018-06-09 17:11:56 -0700

    Add function for getting project ID from project code

--- FinalProject/WebContent/javascript/database.js
+++ FinalProject/WebContent/javascript/database.js
@@ -51,3 +51,21 @@ function assignTaskInDatabase(userID, taskID) {
 		}
 	});
 }
+
+function getProjectID() {
+	var url = "loadProjectData.java";
+	var type = "getProjectID";
+
+	$.ajax({
+		type : "POST",
+		data : 'queryType=' + type + '&userID=' + userID + 'taskID=' + taskID,
+		url : url,
+
+		// Runs once the request returns
+		success : function(content) {
+			console.log("got project ID from database");
+			return content.getParameter("projectID");
+			// 	        		sendUpdateToAllOtherUsers();
+		}
+	});
+}
